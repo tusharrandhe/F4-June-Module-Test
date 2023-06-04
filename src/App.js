0c14a3e81d9a88f174902ce3344c7ddcfdbf58e9
Govind <agovindchavanb@gmail.com>
2023-06-04 22:14:14 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Home from "./components/Home/Home";
+import Detail from "./components/Details/Detail";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/item/:id" component={Detail} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Home from "./components/Home/Home";
+import Detail from "./components/Details/Detail";
+import Navbar from "./components/Navbar";
+import "./index.css";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/item/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
