9b3b6ab516675cf373757b79bbc0de4ce5e3b680
Govind <agovindchavanb@gmail.com>
2023-06-04 22:20:20 +0530

    updated Code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Home from "./components/Home/Home";
